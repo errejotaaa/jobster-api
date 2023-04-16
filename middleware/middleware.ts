1929703d2eb3778a521ad0bdd354db52f8d6c6ce
errejotaaa <ricardojaa.91@gmail.com>
2023-04-16 02:09:50 -0400

    done

--- middleware/middleware.ts
+++ middleware/middleware.ts
@@ -4,7 +4,7 @@ import helmet from "helmet";
 import xss from "xss-clean";
 
 export const middleware = [
-  express.static(path.resolve(__dirname, "./client/build")),
+  express.static(path.resolve(__dirname, "../client/build")),
   express.json(),
   helmet(),
   xss(),
